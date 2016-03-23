e1adfb1ad65c2487d539879e8548a9a97e0bca7f
Joshua Davis <karbonbased@gmail.com>
2016-03-23 17:08:18 -0400

    set up routes for index and test in controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+
+	def index
+		puts "=============="
+		p $petfinder
+		puts "=============="
+		@pets = $petfinder.find_pets('dog', '11211', count:10)
+		@pets.count
+		puts "====@pets is=========="
+		puts @pets.to_xml
+		puts "=============="
+	end
+
+	def show
+	end
+
+	def test
+		@dog = $petfinder.pet('31564331')
+		respond_to do |format|
+    		format.html
+    		format.json{
+     	 	render :json => @dog.to_json
+  		  }
+  		end
+	end
+	
 end
