6915981e1d1b78372d0b6af9bbc935d9e302d480
Joshua Davis <karbonbased@gmail.com>
2016-03-23 17:05:36 -0400

    creating initializer for petfinder config,  now available globally

---
+++ config/initializers/petfinder.rb
@@ -0,0 +1,6 @@
+Petfinder.configure do |config|
+  config.api_key = Rails.application.secrets.petfinder_key
+  config.api_secret = Rails.application.secrets.petfinder_secret
+end
+
+$petfinder = Petfinder::Client.new
