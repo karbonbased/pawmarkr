09fcfcd7b56afb2643b4fbfbe429b3aa19be4af1
Joshua Davis <karbonbased@gmail.com>
2016-03-23 19:18:03 -0400

    filling out migration file with lat, long, name, address

--- db/migrate/20160323231517_create_maps.rb
+++ db/migrate/20160323231517_create_maps.rb
@@ -1,6 +1,10 @@
 class CreateMaps < ActiveRecord::Migration
   def change
     create_table :maps do |t|
+    	t.float :lat
+    	t.float :long
+    	t.string :name
+    	t.text :address
 
       t.timestamps null: false
     end
